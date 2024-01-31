adccae601a69e7eade03eaea6b6f0d4bd1eaf69f
JoaoPaulo <jpaulofsantos@gmail.com>
2024-01-30 21:25:43 -0300

    product repository tests

---
+++ backend/src/test/java/com/developer/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.developer.dscatalog.repositories;
+
+import com.developer.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    private Long existingId;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        existingId = 1L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        productRepository.deleteById(existingId);
+
+        Optional<Product> result = productRepository.findById(existingId);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+
+}
